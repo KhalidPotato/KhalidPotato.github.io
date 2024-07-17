fa45f82045ef05571dffd98fb61c742e51226329
KhalidPotato <robuxover@gmail.com>
2024-07-17 21:23:10 +0400

    Create rightclick.js

---
+++ rightclick.js
@@ -0,0 +1,35 @@
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.ctrlKey && (e.key === 'u' || e.key === 'U' || e.key === 'i' || e.key === 'I')) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && (e.key === 'i' || e.key === 'I')) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && (e.key === 'c' || e.key === 'C')) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && (e.key === 'j' || e.key === 'J')) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && (e.key === 'c' || e.key === 'C')) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && (e.key === 'j' || e.key === 'J')) {
+    e.preventDefault();
+  }
+  if (e.key === 'F12') {
+    e.preventDefault();
+  }
+});
+
+document.addEventListener('dragstart', function (e) {
+  e.preventDefault();
+});
+
+window.addEventListener('resize', function (e) {
+  window.resizeTo(window.outerWidth, window.outerHeight);
+});
